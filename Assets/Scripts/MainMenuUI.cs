620d6d35c01f3542276b155f74d914eec585fe1e
Krishnanand Yadav <krishnanand2517@gmail.com>
2022-08-25 18:38:01 +0530

    Finished

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class MainMenuUI : MonoBehaviour
 {
+    public TextMeshProUGUI HighScoreText;
+
+    private int highScore;
+    private string playerName;
+
+    private void Start()
+    {
+        GameManager.Instance.LoadScore();
+        UpdateHighScoreText();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -20,5 +32,13 @@ public class MainMenuUI : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    private void UpdateHighScoreText()
+    {
+        highScore = GameManager.Instance.HighScore;
+        playerName = GameManager.Instance.BestPlayer;
+
+        HighScoreText.text = "Best Score: " + playerName + ": " + highScore;
     }
 }
